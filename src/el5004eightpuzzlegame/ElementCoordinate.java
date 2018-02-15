04c5dd9528a39e927d22eba202973c0b110ee920
hibagus <git@bagus.my.id>
2018-02-15 09:05:35 +0700

    Initial Commit

---
+++ src/el5004eightpuzzlegame/ElementCoordinate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package el5004eightpuzzlegame;
+
+/**
+ *
+ * @author Bagus Hanindhito
+ * The ElementCoordinate class define the coordinate of each element in the puzzle
+ * in the form of horizontal (integer) and vertical (integer) coordinate.
+ * The origin (0,0) is started on the left-bottom side.
+ * 
+ */
+public class ElementCoordinate
+{
+    
+}
